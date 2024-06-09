280bc93b466c08cf533ce2991d48b81380ffdb45
mahbubreza <mahbubrezachoudhury@gmail.com>
2024-06-09 23:51:37 +0600

    commit from home, 9th June, 2024

--- src/components/PostSelector.jsx
+++ src/components/PostSelector.jsx
@@ -4,18 +4,19 @@ const resource = fetchPosts(
   "https://jsonplaceholder.typicode.com/posts?_limit=5"
 );
 export default function PostSelector({ onSelectPost }) {
-  throw resource;
+  const posts = resource.read();
   return (
     <div>
-      <div>After Resolve</div>
-      {/* <select onChange={onSelectPost}>
-        <option value="">Select Post</option>
-        {posts.map((post) => (
-          <option key={post.id} value={post.id}>
-            {post.title}
-          </option>
-        ))}
-      </select> */}
+      {
+        <select onChange={onSelectPost}>
+          <option value="">Select Post</option>
+          {posts.map((post) => (
+            <option key={post.id} value={post.id}>
+              {post.title}
+            </option>
+          ))}
+        </select>
+      }
     </div>
   );
 }
